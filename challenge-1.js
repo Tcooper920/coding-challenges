f405057b723b67cc7add70d734253fbe2932db29
Thomas Cooper <thomascooper@Thomass-MacBook-Pro.local>
2020-03-03 18:12:26 -0500

    Finished the first challenge.

---
+++ challenge-1.js
@@ -0,0 +1,8 @@
+// 1. Write a function that takes three arguments - all numbers - adds them together, split the result in 3 and displays the result on the console.
+
+function addDivide(numOne, numTwo, numThree) {
+	var equation = ((numOne + numTwo + numThree)/3);
+	console.log(equation);
+}
+
+addDivide(3, 3, 3);
