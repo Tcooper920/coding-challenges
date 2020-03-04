0b3929db82040a5fd9ebd23972b13377a086708e
Thomas Cooper <thomascooper@Thomass-MacBook-Pro.local>
2020-03-03 19:15:45 -0500

    Challenge 1 done

--- challenge-1.js
+++ challenge-1.js
@@ -1,25 +1,25 @@
 // 1. Write a function that takes three arguments - all numbers - adds them together, split the result in 3 and displays the result on the console.
 
-// function addDivide(numOne, numTwo, numThree) {
-// 	var equation = ((numOne + numTwo + numThree)/3);
-// 	console.log(equation);
-// }
+function addDivide(numOne, numTwo, numThree) {
+	var equation = ((numOne + numTwo + numThree)/3);
+	console.log(equation);
+}
 
-// addDivide(3, 3, 3);
+addDivide(3, 3, 3);
 
 // 2. Write a function that takes an array of numbers as an argument. Then it lists ONLY the numbers that are divisible by 3  If there are not such numbers console.log(“Sorry, no 3-divisible numbers”). (Think of push() method)
 
-var newListArray = [];
+// var newListArray = [];
 
-function listNumbers(myArray) {
-	for (var i = 0; i < myArray.length; i++) {
-		if (myArray[i] % 3 === 0) {
-			newListArray.push(myArray[i]);
-		}
+// function listNumbers(myArray) {
+// 	for (var i = 0; i < myArray.length; i++) {
+// 		if (myArray[i] % 3 === 0) {
+// 			newListArray.push(myArray[i]);
+// 		}
 
-	}
-	console.log(newListArray);
-}
+// 	}
+// 	console.log(newListArray);
+// }
 
-listNumbers([1,2,3,4,5,6,7,8,9,10]);
+// listNumbers([1,2,3,4,5,6,7,8,9,10]);
 
